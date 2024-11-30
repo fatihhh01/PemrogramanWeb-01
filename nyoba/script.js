9a70aeecbc6814b475a0e5f4d97a4884989ed1ef
fatihhh01 <fatihassofwan02@gmail.com>
2024-11-30 08:34:13 +0700

    Delete nyoba directory
    hapus file nyoba

--- nyoba/script.js
+++
@@ -1,7 +0,0 @@
-const btnHamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-
-btnHamburger.addEventListener("click", () => {
-    btnHamburger.classList.toggle("is-active");
-    menu.classList.toggle("menu-active");
-});
